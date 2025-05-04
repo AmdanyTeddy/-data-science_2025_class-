a315adcb1baf61e34e8d21c5ef32a2d8b0674282
AmdanyTeddy <amdanyteddy@gmail.com>
2025-05-04 19:25:30 +0300

    Add files via upload
    Teddy Assignment 2

---
+++ Submissions/assignments/TeddyAss2.py
@@ -0,0 +1,13 @@
+# Calculating the Volume and Surface Area of a closed Cylinder
+# Define the radius and height of the closed cylinder
+radius = 3.0
+height = 5.0
+
+# Calculate the Volume of the Cylinder
+volume = 3.14159 * radius ** 2 * height           
+
+# Calculate the Surface Area of the closed Cylinder
+surface_area = 2 * 3.14159 * radius * (radius + height)
+
+# Print the Volume and Surface Area
+print("Volume:", volume,  "Surface Area:", surface_area)
